6513553300a3be08dd08c4de4dfc542b0a6504f8
Nipapan Aom <nipapan@maqe.com>
2019-12-09 12:44:47 +0700

    Done security chapter.

--- server.js
+++ server.js
@@ -8,13 +8,29 @@ const bodyParser = require('body-parser');
 const app = express();
 
 // Middleware
-app.use(cors());
 app.use(bodyParser.json());
 app.use(helmet());
 app.use(morgan('tiny'));
+// Middleware - CORS
+const whiteList = ['http://localhost:8000', 'https://nipapan.github.io'];
+const corsOptions = {
+   origin: function (origin, callback) {
+      if(whiteList.indexOf(origin) !== -1) {
+         callback(null, true);
+      } else {
+         callback(new Error('Not allowed by CORS'));
+      }
+   }
+}
+app.use(cors(corsOptions));
 
 //Routes
-app.get('/', (req, res) => res.send('Hello World'));
+app.get('/', (req, res) => {
+   res.set({
+      'Content-Security-Policy': "script-src 'self' 'https://apis.google.com'"
+   });
+   res.send('Hello World');
+});
 app.post('/secret', (req, res) => {
    const {userMsg} = req.body;
    if (userMsg) {
